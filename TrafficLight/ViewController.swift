f293600c90f0f2838c286949b5503ffc4013bc56
Artur Sokolov <barhishtan@gmail.com>
2019-07-24 18:33:04 +0200

    Add properties and methods

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lighterViews: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    @IBAction func startLighter(_ sender: UIButton) {
+    }
+    
 }
 
