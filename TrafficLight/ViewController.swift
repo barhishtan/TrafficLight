e5bb1bc0c8a6dbff02b6804f5f706a1a377df720
Artur Sokolov <barhishtan@gmail.com>
2019-07-24 20:16:31 +0200

    Final Version

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -11,13 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var lighterViews: [UIView]!
-    @IBOutlet weak var startButton: UIButton!
+    private var lighterID = 0
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        for view in lighterViews {
+            view.layer.cornerRadius = view.bounds.size.width * 0.5
+            view.clipsToBounds = true
+            view.alpha = 0.3
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lighterViews[0].backgroundColor = .red
+        lighterViews[1].backgroundColor = .yellow
+        lighterViews[2].backgroundColor = .green
     }
     
     @IBAction func startLighter(_ sender: UIButton) {
+        sender.setTitle("Next", for: .normal)
+        let viewsMaxIndex = lighterViews.count - 1
+        
+        switch lighterID {
+        case 0:
+            lighterViews[viewsMaxIndex].alpha = 0.3
+        case 1...viewsMaxIndex:
+            lighterViews[lighterID - 1].alpha = 0.3
+        default:
+            break
+        }
+        
+        lighterViews[lighterID].alpha = 1
+        lighterID += 1
+        if lighterID > viewsMaxIndex { lighterID = 0 }
     }
     
 }
