c6013271b416af7216ba489fdbc213249a9dd88c
Artur Sokolov <barhishtan@gmail.com>
2019-07-29 17:17:49 +0200

    Небольшие исправления

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -11,15 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var lighterViews: [UIView]!
-    private var lighterID = 0
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    private var lighterID = 0
+    private let offLighter: CGFloat = 0.3
+    private let onLighter: CGFloat = 1
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
         for view in lighterViews {
-            view.layer.cornerRadius = view.bounds.size.width * 0.5
-            view.clipsToBounds = true
-            view.alpha = 0.3
+            view.layer.cornerRadius = view.frame.width * 0.5
+            view.alpha = offLighter
         }
     }
     
@@ -37,14 +39,14 @@ class ViewController: UIViewController {
         
         switch lighterID {
         case 0:
-            lighterViews[viewsMaxIndex].alpha = 0.3
+            lighterViews[viewsMaxIndex].alpha = offLighter
         case 1...viewsMaxIndex:
-            lighterViews[lighterID - 1].alpha = 0.3
+            lighterViews[lighterID - 1].alpha = offLighter
         default:
             break
         }
         
-        lighterViews[lighterID].alpha = 1
+        lighterViews[lighterID].alpha = onLighter
         lighterID += 1
         if lighterID > viewsMaxIndex { lighterID = 0 }
     }
